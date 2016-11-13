2592789847fc0b336699a822992a75948237d750
ShawnSwinsick <vifs888@gmail.com>
2016-11-13 16:20:40 -0700

    First Check in, Should be working

---
+++ SoundCloudViewer/SoundCloudViewer/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace SoundCloudViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static String SongName = "";
+        public static String SongInfo = "";
+        private SoundCloud SoundCloud;
+        private int MainWidth = 300;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SoundCloud = new SoundCloud();
+            var timer = new DispatcherTimer();
+            var temp = new BackgroundWorker();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += new EventHandler(Loop);
+            timer.Start();
+            //UpdateUI();
+        }
+
+
+
+        private void StartLoopingName()
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = canMain.ActualWidth;
+            doubleAnimation.To = -txtSongName.ActualWidth;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            doubleAnimation.Duration = new Duration(TimeSpan.Parse("0:0:8"));
+            txtSongName.BeginAnimation(Canvas.LeftProperty, doubleAnimation);
+        }
+
+        private void EndLoopingName()
+        {
+            txtSongName.BeginAnimation(Canvas.LeftProperty, null);
+            txtSongName.Width = MainWidth;
+        }
+
+        private void StartLoopingInfo()
+        {
+            DoubleAnimation doubleAnimation = new DoubleAnimation();
+            doubleAnimation.From = canMain.ActualWidth;
+            doubleAnimation.To = -txtSongInfo.ActualWidth;
+            doubleAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            doubleAnimation.Duration = new Duration(TimeSpan.Parse("0:0:8"));
+            txtSongInfo.BeginAnimation(Canvas.LeftProperty, doubleAnimation);
+        }
+
+        private void EndLoopingInfo()
+        {
+            txtSongInfo.BeginAnimation(Canvas.LeftProperty, null);
+            txtSongInfo.Width = MainWidth;
+        }
+
+        private void Loop(object sender, EventArgs e)
+        {
+            SoundCloud.GetInfo();
+            UpdateUI();
+        }
+        
+
+        private void UpdateUI()
+        {
+
+            var tempName = txtSongName.Text;
+            var tempInfo = txtSongInfo.Text;
+            if (tempName != SongName && tempInfo != SongInfo)
+            {
+                EndLoopingInfo();
+                EndLoopingName();
+                txtSongName.Text = SongName;
+                txtSongInfo.Text = SongInfo;
+                if (SongName.Count() > 31)
+                {
+                    txtSongName.Width = SongName.Count() * 9.7;
+                    StartLoopingName();
+                }
+                if(SongInfo.Count() > 39)
+                {
+                    txtSongInfo.Width = SongInfo.Count() * 7.6;
+                    StartLoopingInfo();
+                }
+            }
+
+
+        }
+    }
+}
---
+++ SoundCloudViewer/SoundCloudViewer/SoundCloud.cs
@@ -0,0 +1,57 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundCloudViewer
+{
+    class SoundCloud
+    {
+        private ChromeDriver Browser;
+
+
+        public SoundCloud()
+        {
+            Browser = new ChromeDriver(@"..\..\Selenium");
+            Browser.Navigate().GoToUrl("https://soundcloud.com/");
+            
+        }
+
+        private IWebElement FindElement(String xpath)
+        {
+            IWebElement temp = null;
+            try
+            {
+                temp = Browser.FindElement(By.XPath(xpath));
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.ToString());
+            }
+            return temp;
+
+        }
+
+        public void GetInfo()
+        {
+            //var player = FindElement("//*/div[@class=\"playControls__inner\"]");
+
+            MainWindow.SongName = "";
+            MainWindow.SongInfo = "";
+
+            if (Browser.Url.ToLower().Contains("soundcloud"))
+            {
+                var songInfo = Browser.Title;
+                if (songInfo.Contains("by"))
+                {
+                    var temp = songInfo.Split(new[] { "by" }, StringSplitOptions.None);
+                    MainWindow.SongName = temp[0].Trim();
+                    MainWindow.SongInfo = temp[1].Trim();
+                }
+            }
+        }
+    }
+}
